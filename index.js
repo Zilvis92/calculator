cef0c6b3f0a826f4a8e5db085a0a2f870b2147ff
zilvinasblinovas <blinovaszilvinas@gmail.com>
2025-06-09 19:23:16 +0300

    veiksmu apskaiciavimas

--- index.js
+++ index.js
@@ -14,3 +14,30 @@ function onActionClick(clickedAction) {
     action = clickedAction;
 }
 
+function onCountClick() {
+    let splitted = input.value.split(' ');
+    firstNumber = parseInt(splitted[0]);
+    action = splitted[1];
+    secondNumber = parseInt(splitted[2]);
+
+    calculateAnswer();
+}
+
+function calculateAnswer() {
+    switch (action) {
+        case '+':
+            answer = firstNumber + secondNumber;
+            break;
+        case '-':
+            answer = firstNumber - secondNumber;
+            break;
+        case '*':
+            answer = firstNumber * secondNumber;
+            break;
+        case '/':
+            answer = firstNumber / secondNumber;
+            break;
+        default:
+            answer = 0;
+    }
+}
